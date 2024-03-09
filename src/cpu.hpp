c24c8613517633750c1ad077e0f4eca8a6cbb2ab
Cuking01 <cuking998244353@gmail.com>
2024-03-10 00:42:39 +0800

    写完朴素双调排序和测试框架

---
+++ src/cpu.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "cpu/native.hpp"
+#include "cpu/simd_avx2.hpp"
+
---
+++ src/cpu/native.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include<algorithm>
+#include<concepts>
+#include<stdint.h>
+
+struct Inc
+{
+	static void swap(int*a,int*b)
+	{
+		int min=std::min(*a,*b);
+		int max=std::max(*a,*b);
+		*a=min;
+		*b=max;
+	}
+};
+struct Dec
+{
+	static void swap(int*a,int*b)
+	{
+		int min=std::min(*a,*b);
+		int max=std::max(*a,*b);
+		*a=max;
+		*b=min;
+	}
+};
+
+template<typename T>
+concept Order_Like=std::same_as<T,Inc>||std::same_as<T,Dec>;
+
+template<Order_Like Order>
+void divide(int*arr,uint64_t len)
+{
+	if(len==1)return;
+	len>>=1;
+	for(uint64_t i=0;i<len;i++)
+		Order::swap(arr+i,arr+len+i);
+	divide<Order>(arr,len);
+	divide<Order>(arr+len,len);
+}
+
+
+//len必须是2的幂次
+template<Order_Like Order>
+void bitonic_sort_native(int*arr,uint64_t len)
+{
+	if(len==1)return;
+
+	len>>=1;
+	bitonic_sort_native<Inc>(arr,len);
+	bitonic_sort_native<Dec>(arr+len,len);
+	divide<Order>(arr,len<<1);
+}
+
+
